f176558451517241196616049926836252e017b7
AhamedThijani-PP <153520061+AhamedThijani-PP@users.noreply.github.com>
2024-02-29 21:01:39 +0530

    Update DHT11.ino
    removed heat index

--- IOT/DHT11.ino
+++ IOT/DHT11.ino
@@ -55,20 +55,11 @@ void loop() {
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
   Serial.print(t);
   Serial.print(F("°C "));
   Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
+  Serial.print(F("°F"));
 }
