b0fdb3d421cd7d4462b88760b3403314592613ef
AhamedThijani-PP <153520061+AhamedThijani-PP@users.noreply.github.com>
2024-04-06 05:04:50 +0530

    Add files via upload

---
+++ Telecom Churn Prediction/streamlit_telecom.py
@@ -0,0 +1,15 @@
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.model_selection import train_test_split as tts
+import pandas as pd
+import streamlit as st
+import joblib
+model=joblib.load(open(telecom_churn_prediction.pkl,'rb'))
+st.title('Telecom Churn Prediction')
+st.write('------------------------------------------------')
+sepal_l=st.number_input(label='Enter the Sepal Length')
+sepal_w=st.number_input(label='Enter the Sepal Width')
+petal_l=st.number_input(label='Enter the Petal Length')
+petal_w=st.number_input(label='Enter the Petal Width')
+pred=model.predict([[sepal_l,sepal_w,petal_l,petal_w]])
+if st.button('Predict Result'):
+    st.success(f'{pred}')
