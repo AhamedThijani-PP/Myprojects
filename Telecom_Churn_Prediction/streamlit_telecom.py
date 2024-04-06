cd290f86dbaf6f21c73dd1008194dc6358085d89
AhamedThijani-PP <153520061+AhamedThijani-PP@users.noreply.github.com>
2024-04-06 09:29:03 +0530

    Update streamlit_telecom.py

--- Telecom_Churn_Prediction/streamlit_telecom.py
+++ Telecom_Churn_Prediction/streamlit_telecom.py
@@ -18,8 +18,8 @@ def render_home():
     model=joblib.load(open(Path,'rb'))
     st.title('Telecom Churn Prediction')
     st.write('------------------------------------------------')
-    gender=st.selectbox('Select Gender(1=Male,0=Female)', ['1', '0'])
-    gender_value = 1 if gender == 'Male' else 0
+    gender = st.selectbox('Select Gender', ['Male', 'Female'])
+    gender_value = '1' if gender == 'Male' else 0
     seniorcitizen=st.selectbox('Are you Senior Citizen?(1 = Yes,0 = No)', ['1', '0'])
     partner=st.selectbox('Are you single?(1 = Yes,0 = No)', ['1', '0'])
     dependents=st.selectbox('Are you depended?(1 = Yes,0 = No)', ['1', '0'])
