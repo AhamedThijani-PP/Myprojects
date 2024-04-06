f84e467624d18670dc7a1ba96fff97fa4c74ed80
AhamedThijani-PP <153520061+AhamedThijani-PP@users.noreply.github.com>
2024-04-06 07:31:43 +0530

    Update streamlit_telecom.py

--- Telecom_Churn_Prediction/streamlit_telecom.py
+++ Telecom_Churn_Prediction/streamlit_telecom.py
@@ -7,7 +7,7 @@ import joblib
 model=joblib.load(open(Path,'rb'))
 st.title('Telecom Churn Prediction')
 st.write('------------------------------------------------')
-gender=st.number_input(label='Enter the Sepal Length')
+gender=st.selectbox('Select Gender', ['Male', 'Female'])
 sepal_w=st.number_input(label='Enter the Sepal Width')
 petal_l=st.number_input(label='Enter the Petal Length')
 petal_w=st.number_input(label='Enter the Petal Width')
