297442a2709b95bf25a0d2f4fa3b8aca9332ec88
AhamedThijani-PP <153520061+AhamedThijani-PP@users.noreply.github.com>
2024-04-06 07:47:25 +0530

    Update streamlit_telecom.py

--- Telecom_Churn_Prediction/streamlit_telecom.py
+++ Telecom_Churn_Prediction/streamlit_telecom.py
@@ -13,7 +13,7 @@ partner=st.selectbox('Are you single?(1 = Yes,0 = No)', ['1', '0'])
 dependents=st.selectbox('Are you depended?(1 = Yes,0 = No)', ['1', '0'])
 tenure=st.slider('Select a tenure', 0, 1300, 50)
 phoneservice=st.selectbox('Select the Phone Service(1 = Yes,0 = No)', ['1', '0'])
-multiplelines=st.selectbox('Select the Phone Service(1 = Yes,0 = No,2='No phone sevice)', ['1', '0','2'])
+multiplelines=st.selectbox('Select the Phone Service(1 = Yes,0 = No,2=No phone sevice)', ['1', '0','2'])
 pred=model.predict([[sepal_l,sepal_w,petal_l,petal_w]])
 if st.button('Predict Result'):
     st.success(f'{pred}')
