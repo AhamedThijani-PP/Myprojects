854c15937d8bc7a63d59b7c65168934aaa8b728b
AhamedThijani-PP <153520061+AhamedThijani-PP@users.noreply.github.com>
2024-04-06 10:06:38 +0530

    Update streamlit_telecom.py

--- Telecom_Churn_Prediction/streamlit_telecom.py
+++ Telecom_Churn_Prediction/streamlit_telecom.py
@@ -31,21 +31,78 @@ def render_home():
     phoneservice='1' if phoneservice == 'Yes' else 0
     multiplelines=st.selectbox('Select the Phone Service', ['Yes','No','No PhoneService'])
     if multiplelines == 'Yes':
-        multiplelines = 1
+        multiplelines = '1'
     elif multiplelines == 'No':
-        multiplelines = 0
+        multiplelines = '0'
     else:
-        multiplelines = 2
-    internetservice=st.selectbox('Select the Internet Service(1 = Fibre,0 = No,2=DSL)', ['1', '0','2'])
-    onlinesecurity=st.selectbox('Select the Online Security(1 = Yes,0 = No,2=No Internet Service)', ['1', '0','2'])
-    onlinebackup=st.selectbox('Select the Online Backup(1 = Yes,0 = No,2=No Internet sevice)', ['1', '0','2'])
-    deviceprotection=st.selectbox('Select the Device Protection(1 = Yes,0 = No,2=No Internet service)', ['1', '0','2'])
-    techsupport=st.selectbox('Select the Tech Support(1 = Yes,0 = No,2=No Internet sevice)', ['1', '0','2'])
-    streamingtv=st.selectbox('Select the StreamingTV(1 = Yes,0 = No,2=No Internet sevice)', ['1', '0','2'])
-    streamingmovies=st.selectbox('Select the StreamingMovies(1 = Yes,0 = No,2=No Internet sevice)', ['1', '0','2'])
-    contract=st.selectbox('Select the contract(1 = One Year,2= Two Year,3=Month to Month)', ['1', '2','3'])
-    paperless=st.selectbox('Is it Paperless Billing?(1 = Yes,0 = No)', ['1', '0'])
-    paymentmethod=st.selectbox('Select the Payment Method(1 = Mailed check,0 = Electronic check,2=Bank Transfer(Automatic),3=credit card', ['1', '0','2','3'])
+        multiplelines = '2'
+    internetservice=st.selectbox('Select the Internet Service', ['No','Fiber','DSL'])
+    if internetservice == 'Fiber':
+        internetservice = '1'
+    elif internetservice == 'No':
+        internetservice = '0'
+    else:
+        internetservice = '2'
+    onlinesecurity=st.selectbox('Select the Online Security', ['Yes','No','No InternetService'])
+    if onlinesecurity == 'Yes':
+        onlinesecurity = '1'
+    elif onlinesecurity == 'No':
+        onlinesecurity = '0'
+    else:
+        onlinesecurity = '2'
+    onlinebackup=st.selectbox('Select the Online Backup', ['Yes','No','No InternetService'])
+    if onlinebackup == 'Yes':
+        onlinebackup = '1'
+    elif onlinebackup == 'No':
+        onlinebackup = '0'
+    else:
+        onlinebackup= '2'
+    deviceprotection=st.selectbox('Select the Device Protection', ['Yes','No','No PhoneService'])
+    if deviceprotection == 'Yes':
+        deviceprotection = '1'
+    elif deviceprotection == 'No':
+        deviceprotection = '0'
+    else:
+        deviceprotection= '2'
+    techsupport=st.selectbox('Select the Tech Support', ['Yes','No','No InternetService'])
+    if techsupport == 'Yes':
+        techsupport = '1'
+    elif techsupport == 'No':
+        techsupport = '0'
+    else:
+        techsupport= '2'
+    streamingtv=st.selectbox('Select the StreamingTV', ['Yes','No','No InternetService'])
+    if streamingtv == 'Yes':
+        streamingtv = '1'
+    elif streamingtv == 'No':
+        streamingtv = '0'
+    else:
+        streamingtv= '2'
+    streamingmovies=st.selectbox('Select the StreamingMovies', ['Yes','No','No InternetService'])
+    if streamingmovies == 'Yes':
+        streamingmovies = '1'
+    elif streamingmovies == 'No':
+        streamingmovies = '0'
+    else:
+        streamingmovies= '2'
+    contract=st.selectbox('Select the contract',['One Year','Two Year','Month to Month'])
+    if contract == 'One Year':
+        contract = '1'
+    elif contract == 'Two Year':
+        contract = '2'
+    else:
+        contract='3'
+    paperless=st.selectbox('Is it Paperless Billing?', ['Yes','No'])
+    partner='1' if partner == 'Yes' else 0
+    paymentmethod=st.selectbox('Select the Payment Method', ['Electronic check','Mailed check','Bank Transfer(Automatic)','credit card'])
+    if paymentmethod == 'One Year':
+        paymentmethod = '1'
+    elif paymentmethod == 'Two Year':
+        paymentmethod = '2'
+    elif paymentmethod='Bank Transfer(Automatic)':
+        paymentmethod='3'
+    else:
+        paymentmethod='4'
     monthlycharges=st.number_input(label='Enter the monthly Charge')
     totalcharges=st.number_input(label='Enter the Total Charge')
     pred=model.predict([[gender,seniorcitizen,partner,dependents,tenure,phoneservice,multiplelines,internetservice,onlinesecurity,onlinebackup,deviceprotection,techsupport,streamingtv,streamingmovies,contract,paperless,paymentmethod,monthlycharges,totalcharges]])
